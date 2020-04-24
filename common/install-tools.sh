be8a721032b1914370b3d64cab9aff53d5ef9169
David Solomon <dlsolomon@dlsolomon-macbookpro.roam.corp.google.com>
2020-04-24 15:40:37 -0400

    Changed Istio version back to 1.4.7

--- common/install-tools.sh
+++ common/install-tools.sh
@@ -18,7 +18,7 @@
 export PROJECT=$(gcloud config get-value project)
 export WORK_DIR=${WORK_DIR:="${PWD}/workdir"}
 
-export ISTIO_VERSION=1.1.4
+export ISTIO_VERSION=1.4.7
 
 ## Install Tools
 mkdir -p $WORK_DIR/bin
@@ -28,8 +28,8 @@ echo "### Begin Tools install"
 echo "### "
 
 ## Install kubectx
-curl -sLO https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx 
-chmod +x kubectx 
+curl -sLO https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx
+chmod +x kubectx
 mv kubectx $WORK_DIR/bin
 
 # Download Istio
@@ -41,7 +41,7 @@ mv istio-$ISTIO_VERSION $WORK_DIR/
 #curl -o yq.v2 -OL https://github.com/mikefarah/yq/releases/download/2.3.0/yq_linux_amd64
 #chmod +x yq.v2
 #mv yq.v2 $WORK_DIR/bin
- 
+
 # Install Kops
 curl -sLO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops-linux-amd64
@@ -51,4 +51,3 @@ mv kops-linux-amd64 $WORK_DIR/bin/kops
 gsutil cp gs://config-management-release/released/latest/linux_amd64/nomos nomos
 chmod +x nomos
 mv nomos $WORK_DIR/bin
-
